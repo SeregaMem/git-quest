e8bbfcd1fa47545349f47f1637269781826749eb
SeregaMem <seregamems@gmail.com>
2022-04-02 22:21:58 +0700

    add file HW

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,11 +1,15 @@
 ﻿
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
-int main
-{
-    cout << "Hello World!\n";
+int main() {
+    ofstream out("output.txt");
+    if (out.is_open()) {
+        out << "Hello World";
+    }
+    return 0;
 }
 
 
